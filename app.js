528cd61b9fa5b993510f2fde23f57e9cace58e29
Alena Buiko <abuiko.ma@gmail.com>
2021-02-02 17:22:10 -0500

    create board with candies

--- app.js
+++ app.js
@@ -7,4 +7,38 @@ document.addEventListener('DOMContentLoaded', () => {
     const width = 8;
     const squares = [];
     let score = 0;
+
+    const candyColors = [
+        'url(img/alternative-blue.png)',
+        'url(img/alternative-red.png)',
+        'url(img/alternative-green.png)',
+        'url(img/alternative-orange.png)',
+        'url(img/alternative-yellow.png)',
+        'url(img/alternative-purple.png)'
+    ]
+
+    function createBoard() {
+        for (let i = 0; i < width * width; i++) {
+            const square = document.createElement('div');
+            square.setAttribute('draggable', true);
+            square.setAttribute('id', i);
+            let randomColor = Math.floor(Math.random() * candyColors.length);
+            square.style.backgroundImage = candyColors[randomColor];
+            grid.appendChild(square);
+            squares.push(square);
+
+        }
+    }
+
+    createBoard();
+
+
+
+
+
+
+
+
+
+
 })
